c6b338dd8ba516b6f40f861f324d6225786f740c
juhoon212 <ws5501@naver.com>
2023-07-07 00:18:09 +0900

    SpringBoot_Error_page_insert By BasicErrorController

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -20,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
+        // 필터에서 DispatcherType.ERROR 을 세팅하면 내부 호출에서 Error 요청이 오면 필터 호출 x
         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
         return filterRegistrationBean;
     }
@@ -29,6 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LogInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "*.ico", "/error"); // 오류페이지 경로
+                //   /error-page/** 을 넣으면 내부 호출의 경우에는 인터셉터 호출 x
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
     }
 }
--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -20,6 +20,13 @@ public class ServletExController {
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404오류!");
     }
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400오류!");
+    }
+
+
+
 
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
